a0ff8a1930c2571170b74dce19b8b16230a69fb9
Luiz Agner <luizagnern@hotmail.com>
2019-06-16 18:41:49 -0300

    Adding load of G file to be used in test cases

--- web/src/server/Server.java
+++ web/src/server/Server.java
@@ -1,21 +1,50 @@
 package server;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
-
-import controller.ImageController;
-import controller.IntegrationController;
+import model.CGNE;
+import model.G;
+import model.H;
+import model.HttpContainer;
+import model.ProcessedImage;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-      HttpServer server = HttpServer.create(new InetSocketAddress(8500), 0);
+    public static void main(String[] args) {
+      float[] f = processSignal();
+      saveImage(f);
+    }
+    
+    private static void saveImage(float[] f) {
+      ProcessedImage pi = new ProcessedImage(f);
+      pi.saveImage();
+    }
+    
+    private static void startHttp() {
+      HttpContainer http = new HttpContainer();
+    }
+    
+    private static float[] processSignal() {
+      Float convergence = Float.parseFloat("1E-4");
+      Runtime runtime = Runtime.getRuntime();
+      int mb = 1024*1024;
+      H.initialize();
+      G g = new G("g-3.txt");
+      
+      CGNE cgne = new CGNE(g.getG());
+      
+      int i = 0;
+      Float res = (float)0;
+      
+      while((res = cgne.iterate()) > convergence && i < 1000) {
+        System.out.println(i + " " + res + " " + ((runtime.totalMemory() - runtime.freeMemory()) / mb));
+        i++;
+      }
 
-      server.createContext("/integration", IntegrationController::integrate);
-      server.createContext("/image/save", ImageController::save);
-      server.createContext("/image/load", ImageController::load);
-      server.createContext("/image/test", ImageController::test);
-      server.start();
-    } 
+      System.out.println("##### Heap utilization statistics [MB] #####");
+      System.out.println("Used Memory:" 
+        + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+      System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+      System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+      
+      return cgne.getF().toArray();
+    }
 }
